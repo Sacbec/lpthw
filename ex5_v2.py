6c832d59a5c3168aa307e21e5a0fbc190a4707b4
Sacbec <sacbech@gmail.com>
2022-12-28 09:10:46 -0600

    commit del archivo que contiene el ejercicio 5 pero modificado por mi porque es la version 2, corresponde el libreo LP3THW

---
+++ ex5_v2.py
@@ -0,0 +1,21 @@
+my_name = 'Sacbec'
+my_surname = 'Hernandez'
+my_age = 28
+my_height = 180 #cms
+my_weight = 80 #kgs
+my_eyes = 'Brown'
+my_hair = 'Brown'
+my_teeth = 'White'
+
+print(f"Let's talk about {my_name} {my_surname}.")
+print(f"He's {round(my_height * 0.393701)} inches tall.")
+print(f"He's {my_height} cms tall.")
+print(f"He's {round(my_weight * 2.20462)} pounds heavy.")
+print(f"He's {my_weight} kgs heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_age + my_weight + my_height 
+
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
