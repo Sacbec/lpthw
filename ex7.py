cab9d01f6036da74de1282007f1f29c25cbeab97
Sacbec <sacbech@gmail.com>
2022-12-28 09:12:21 -0600

    commit al archivo que contiene el ejercicio 7 del libro LP3THW

---
+++ ex7.py
@@ -0,0 +1,24 @@
+print("Mary had a little lamb.") # imprimo un string
+print("Its fleece was white as {}.".format('snow')) # imprimo un string usando format para meter otro string
+print("And everywhere that Mary went.") #imprimo un string
+print("." * 10) # what´d that do? 
+# i think the line before will print 10 points 
+# confirmo: imprimi 10 veces un punto, usando asterisk
+end1 = "C" # defino una variable con un string
+end2 = "h" # defino una variable con un string usando double quotes
+end3 = "e" # defino una variable con un string usando double quotes
+end4 = "e" # defino una variable con un string usando double quotes
+end5 = "s" # defino una variable con un string usando double quotes
+end6 = "e" # defino una variable con un string usando double quotes
+end7 = "B" # defino una variable con un string usando double quotes
+end8 = "u" # defino una variable con un string usando double quotes
+end9 = "r" # defino una variable con un string usando double quotes
+end10 = "g" # defino una variable con un string usando double quotes
+end11 = "e" # defino una varibale con un string usando double quotes
+end12 = "r" #defino una variable con un string usando double quotes
+
+#watch that commma at the end, try to remove it
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ') # concateno las variables que contienen strings
+# al final uso end y le doy un espacio en blanco en lugar de un salto de linea como lo tiene por defecto
+# es decir que va a imprimir en la misma linea lode ambos print, sin que los mande a dos lineas diferentes
+print(end7 + end8 + end9 + end10 + end11 + end12)
