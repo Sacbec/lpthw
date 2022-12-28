c19aeea3b468cf0b36250aafe31d60795ab1eafa
Sacbec <sacbech@gmail.com>
2022-12-28 09:08:24 -0600

    commit del archivo que contiene el ejercicio 3 del libro LP3THW

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:") # se imprime un enunciado
+
+print("Hens", 25+30/6) # dentro del print tengo un enunciado pero tambien una operacion con numeros
+print("Roosters", 100-25*3%4) # dentro del print tengo un enunciado pero tambien una operacion con numeros y otros operandos 
+
+
+print("Now I will count the eggs:") # imprimo un enunciado
+
+print(3+2+1-5+4%2-1/4+6) # evaluo una operacion pero el resultado es un booleano
+
+print("Is it true that 3+2 < 5-7 ?") # como la operacion la tengo dentro de las comillas, no se evalua
+print(3+2<5-7) # la operacion no tiene comillas por lo que e evalua, pero ademas el resultado es un booleano
+
+print("What is 3+2?", 3+2) # imprimo un enunciado y evaluo una operacion
+
+print("What is 5-7?", 5-7) # imprimo un enunciado y evaluo una operacion
+
+print("Oh, that's why it's False") # imprimo un enuncuado
+
+print("How about some more.") # imprimo un enunciado
+
+print("Is it greater?", 5>-2) # imprimo un enunciado y el resultado de la operacion esta en booleanos
+print("Is it greater or equal?", 5 >= -2) # imprimo un enunciado y el resultado de la operacion esta en boolenanos
+print("Is it less or equal?", 5 <= -2) # imprimo un enunciado y el resultado de la operacion esta en booleanos
