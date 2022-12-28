a6f724d1fd748d917b237735e86d7a862f58befe
Sacbec <sacbech@gmail.com>
2022-12-28 09:11:54 -0600

    commit al archivo que contiene el ejercicio 6 pero con mis modificaciones porque es la version 2, vienen del libro LP3THW

---
+++ ex6_v2.py
@@ -0,0 +1,29 @@
+types_of_people = 10 # declaro una variable 
+x = f"here are {types_of_people} types o people." # con f de format al inicio del string puedo
+# insertar variables definidas anteriormente
+
+binary = "binary" # defino una variable con un string
+do_not = "don't" # defino otra variable con un string
+y = "Those who know {binary} and those who {do_not}." # al pasar f de format puedo meter todas las 
+# variables que yo quiera a mi string
+
+print(x) # imprimir la variable x que ademas esta jalando con f de format variables definidas 
+#previament
+print(y) # imprimir la vriable y que ademas esta jalando con f de format variables definidas 
+#previamente
+
+print(f"I said: {x}") # con f e format estoy jalando la variable x, que a su vez esta jalando otra variable
+print(f"I also said: '{y}'") #con f de format estoy jaland la variable y, que a su vez esta jalando 
+# otra variable 
+
+hilarious = False # defino una variable con un booleano
+joke_evaluation = "Isn't that oke so funny?! {}" # defino una variable con un string que contiene {}
+# puedo insertar variable en strings que contienen {}
+
+
+print(joke_evaluation.format(hilarious)) # FORMAT SE USA PARA STRINGS YA CREADAS
+
+w = "This is the left side of.." #creo na varable con un string
+e = "a string with a right side." # creo una variable con un string
+
+print(w + e) # a través de print puedo sumar o concatenar cadenas o strings
