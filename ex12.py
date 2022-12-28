eab361a5055c8ab2ccc08f872f778358448f52d5
Sacbec <sacbech@gmail.com>
2022-12-28 09:05:04 -0600

    commit del archivo que contiene el ejercicio 12 del libro LP3THW

---
+++ ex12.py
@@ -0,0 +1,7 @@
+age = int(input("How old are you? "))
+height = int(input("How tall are you? "))
+weight = int(input("HOw much do you weight? "))
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+print("How old are you?", input('esto se ejecuta primero'))
