3934ffc4aa7c048ee1d64c9d30b8a249fc7f9361
Sacbec <sacbech@gmail.com>
2022-12-30 20:07:00 -0600

    commit a modificaciones sobre el archivo 15

--- ex15.py
+++ ex15.py
@@ -1,13 +1,18 @@
 from sys import argv
 
 script, filename = argv # nombre del script,
-# nombre del archivo a leer
+                        # nombre del archivo a leer
 
 txt = open(filename) # variable que guarda el resultado de open
-
+# en este punto estoy creando un objeto open. 
+txt.close()
 print(f"Here's your file {filename}:") # imprime el nombre del
 # archivo
 print("Type the filename again:")
 file_again = input("> ")
 
-txt_again = open(file_again)
+txt_again = open(file_again) # guardo el contenido de open
+
+print(txt_again.read()) # .read() sobre la variable que trae el contenido de open 
+
+txt_again.close()
