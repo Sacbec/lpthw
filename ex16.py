66d2e0be8a98066cb3d88fa2233f758020e4a460
Sacbec <sacbech@gmail.com>
2022-12-30 20:34:14 -0600

    comienza prueba para write, truncate y creacion de nuevo  ex16_sample.txt

---
+++ ex16.py
@@ -0,0 +1,34 @@
+from sys import argv 
+
+script, filename = argv # jala el nombre del archivo desde que lo ejecuto y necesito
+# darle el nombre del archivo que va a darle truncate
+
+print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w') # abre el archivo para escribir sobre el y primero lo da truncate
+
+print("Truncating the file. Goodbye!")
+target.truncate() # ya esta abierto el archivo, le esta dando truncate 
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
+print("I'm going to write these to the file.")
+
+target.write(line1) # a traves de .write() esta escribiendo strings en el archivo
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("And finally, we close it.")
+target.close()
