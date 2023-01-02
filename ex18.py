cb4ad46e518f4a2bcaeadfc1d4e5b486fa20b0bd
Sacbec <sacbech@gmail.com>
2023-01-02 11:04:09 -0600

    corrijo typos

--- ex18.py
+++ ex18.py
@@ -2,7 +2,7 @@
 def print_two(*args): # lo interesante aqui es que le puedo pasar todos los paramentros que 
     # yo quiera!!!!
     arg1, arg2 = args 
-    print(f"arg1: {arg1}, arg2_ {arg2}")
+    print(f"arg1: {arg1}, arg2: {arg2}")
 
 #ok, that *args is actually pointless, we can just do this
 def print_two_again(arg1, arg2): # funcion que se llama print_two_again() y recibe dos argumentos
@@ -10,7 +10,7 @@ def print_two_again(arg1, arg2): # funcion que se llama print_two_again() y reci
 
 # this just takes one argument 
 def print_one(arg1): # funcion que se llama print_one y recibe un solo parametro
-    print(f"arg1 {arg1}") # imprime el nombre de la variable y su valor
+    print(f"arg1: {arg1}") # imprime el nombre de la variable y su valor
 
 #this one takes no arguments
 def print_none(): # funcion que se llama print_none y no recibe argumentos
