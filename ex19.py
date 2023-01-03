69036db071231ae95424c3c5d5b5a3f1b95e8133
Sacbec <sacbech@gmail.com>
2023-01-03 17:26:49 -0600

    agrego ex19.py al repo con commit

---
+++ ex19.py
@@ -0,0 +1,10 @@
+def cheese_and_crackers(cheese_count, boxes_of_cranckers):
+    print(f"You have {cheese_count} cheeses!")
+    print(f"You have {boxes_of_cranckers} boxes of crancerks !")
+    print("Man that's enough for a party!")
+    print("Get a blanket. \n")
+
+# nota: como la funcion solo imprime los parametros que recibe, pueden ser numericos o de tipo string
+
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20, 30)
