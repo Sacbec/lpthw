a65164fd24c808958dd146bc241367bd41ced61c
Sacbec <sacbech@gmail.com>
2023-01-12 15:46:48 -0600

    guarde el cambio de las dos primeras funciones agregadas

---
+++ ex21_v2.py
@@ -0,0 +1,21 @@
+# para crear una funcion necesito usar la palabra reservada def
+# luego pondo el nombre que tendra la funcion 
+# abro parentesis
+# le paso los parametros de los que va a depender la funcion, separados por comas
+# cierro parentesis
+# pongo dos puntos 
+def add(numero1, numero2):
+    print(f"Estoy haciendo la suma de {numero1} y {numero2}")
+    return numero1 + numero2 
+# notar que no imprimo nada con el return, unicamente uso return para almacenar el resultado de la operacion, en este caso de una suma
+
+# para crear una funcion uso la pabra reservada def
+# despues escribo el nombre que tendra la funcion 
+# abro parentesis
+# le paso los parametros de los que dependera la funcion, separados por comas
+# cierro parentesis
+# escribo dos puntos
+
+def  subtract(numero1, numero2):
+    print(f"Estoy haciendo la resta de {numero1} - {numero2} ") 
+    return numero1 - numero2 
