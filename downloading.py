ee21eecf09aba5fa2490ea820f7ab8b752457d6d
Sacbec <sacbech@gmail.com>
2023-01-21 15:03:37 -0600

    agrego el resto del script para que pueda descargar archivos desde URL, tambien ya descargo el archivo desde la URL y lo guardo en el mismo directorio que el script que ejecute

--- downloading.py
+++ downloading.py
@@ -1,1 +1,26 @@
 import requests 
+
+
+URL = "https://learnpythonthehardway.org/python3/languages.txt"
+response = requests.get(URL) # decarga la informacion desde la URL
+print(response)
+open("languages.txt", "wb").write(response.content)
+
+# pregunta: ¿donde lo va a guardar?
+    # respuesta: lo guarda en el mismo directorio que mi script de python
+
+# felicidades: logré descargar un archivo txt desde una URL usando python.
+
+def downloader(online_path, file_name_user):
+    URL = online_path 
+    response = requests.get(URL)
+    open(file_name_user, "wb").write(response.content)
+# NOTA: EL NOMBRE DEL ARCHIVO DEBE CONTENER LA EXTENSION PARA PODER SER ABIERTO. !!!
+
+downloader('https://www.elsoldetijuana.com.mx/incoming/7h4hk6-kesikesiperrito.jpg/alternates/LANDSCAPE_768/Kesikesiperrito.jpg'
+, 'perrito_meme')
+
+
+# entonces el codigo inicial lo puedo meter en una función para
+# 1. ejecutarla en el momento que yo quiera 
+# 2. llamarla cuantas veces yo quiera para archivos distintos. 
